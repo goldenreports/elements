3304291e97d0fbb8ef774ab807b087c8a42ee8ee
Mariano Santoro <santoromariano88@gmail.com>
2023-01-10 12:38:27 -0600

    Fix vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,29 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@lib': 'src'
+    }
+  },
   build: {
     lib: {
-      entry: 'src/public_api.ts',
+      name: 'GoldenReportsElements',
+      fileName: 'elements',
+      entry: 'src/elements.ts',
       formats: ['es'],
     },
     minify: false,
+    sourcemap: true,
     rollupOptions: {
-      external: [/^lit/],
+      external: [
+        /^(@)?lit/,
+        /^(@)?codemirror/,
+        /^@cls/,
+        /^@clr/,
+        'interactjs',
+        'yaml'
+      ]
     },
   },
   server: {
